ad7108918c3249fc9b5b3e265cf7b31159e41714
Petar Basta <petar.basta@hotmail.com>
2020-12-13 13:16:13 -0800

    Format App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -67,181 +67,6 @@ class App extends Component<{}, BridgeState> {
     }
   }
 
-  openSettings() {
-    this.setState({showSettings: true}, () => document.removeEventListener("keydown", this.keydown, false))
-  }
-
-  closeSettings() {
-    this.setState({showSettings: false}, () => document.addEventListener("keydown", this.keydown, false))
-  }
-
-  updateConfig(numberDelay: number, problemDelay: number) {
-    this.setState({
-      numberDelay: numberDelay, 
-      problemDelay: problemDelay,
-    })
-  }
-
-  // When key is pressed
-  keydown(event: any) {
-    let keyPressed = event.keyCode
-    // Only allow 0-9 and q,w,e,r
-    if (!this.state.answersLocked && keyPressed >= 48 && keyPressed <= 57) {
-      this.submit(keyPressed - 48)
-    }
-    if (!this.state.answersLocked && keyPressed === 81) {
-      this.submit(10)
-    }
-    if (!this.state.answersLocked && keyPressed === 87) {
-      this.submit(11)
-    }
-    if (!this.state.answersLocked && keyPressed === 69) {
-      this.submit(12)
-    }
-    if (!this.state.answersLocked && keyPressed === 82) {
-      this.submit(13)
-    }
-  }
-
-  render() {
-    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-    const answerButtons = numbers.map((key) => {
-      return (
-        <button disabled={this.state.answersLocked} style={{ margin: "0.5%", height: "90%", width: "9%" }} key={key} onClick={() => this.submit(key)}>
-          {key}
-        </button>
-
-      )
-    })
-    let messageColor = this.state.message === "Correct!" ? "green" : "red"
-    let pointsForProblem = this.state.pointsForProblem === null ? "" : this.state.pointsForProblem + " points"
-    return (
-      <>
-        {/* Displays on PC  */}
-        <BrowserView>
-          <div style={{ position: "absolute", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "100%", width: "100%" }}>
-            <div style={{ alignContent: "center", width: "60%", height: "40%", display: "flex", flexDirection: "column", backgroundColor: "darkgray", border: "2px solid black" }}>
-              {/* Header */}
-              <div style={{justifyContent: "space-between", alignItems: "center", width: "100%", height: "20%", display: "flex", flexDirection: "row", backgroundColor: "gray" }}>
-                <p>Basta Bridge | Counting Game :)</p>
-                {/* Settings button */}
-                <img style={{padding: "3%", height: "45%"}} alt="settings button" src={gear} onClick={() => this.openSettings()} />
-              </div>
-
-              <hr style={{ width: "100%", margin: "0px", border: "1px solid black" }} />
-
-              <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "80%", width: "100%" }}>
-
-                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100%", width: "60%" }}>
-
-                  {/* Problem display */}
-                  <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "40%", width: "100%" }} >
-                    <div className="number-box">
-                      <h2>{this.state.num1}</h2>
-                    </div>
-                    <div className="number-box">
-                      <h2>{this.state.num2}</h2>
-                    </div>
-                    <div className="number-box">
-                      <h2>{this.state.num3}</h2>
-                    </div>
-                    <div className="number-box">
-                      <h2>{this.state.answer}</h2>
-                    </div>
-                  </div>
-
-                  {/* Answer buttons  */}
-                  <div style={{ margin: "0%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "15%", width: "100%" }}>
-                    {answerButtons}
-                  </div>
-
-                  {/* Message on answer */}
-                  <div style={{ margin: "0%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "25%", width: "100%" }}>
-                    <h2 style={{ color: messageColor }}>{this.state.message + " " + pointsForProblem}</h2>
-                  </div>
-
-                  {/* Control buttons */}
-                  <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "20%", width: "100%" }}>
-                    <button disabled={this.state.newProblemsLocked || this.state.problemsRemaining > 0} className="btn button" onClick={() => this.onStart()}>
-                      New problem
-                    </button>
-                    <button disabled={this.state.newProblemsLocked || this.state.problemsRemaining > 0} className="btn button" onClick={() => this.tenProblems()}>
-                      10 Problems
-                    </button>
-                    <button disabled={this.state.clearScoreLocked || this.state.problemsRemaining > 0} className="btn button" onClick={() => this.clearScore()}>
-                      Clear Score
-                    </button>
-                  </div>
-                </div>
-
-                {/* Stats display */}
-                <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "100%", width: "40%" }} >
-                  <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "flex-end", height: "100%", width: "50%" }} >
-                    <p style={{ margin: "4%", padding: "0%" }}>Correct answers:</p>
-                    <p style={{ margin: "4%", padding: "0%" }}>Incorrect answers:</p>
-                    <p style={{ margin: "4%", padding: "0%" }}>Correct percentage:</p>
-                    <p style={{ margin: "4%", padding: "0%" }}>Total points:</p>
-                    <p style={{ margin: "4%", padding: "0%" }}>Average points:</p>
-                  </div>
-
-                  <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "flex-start", height: "100%", width: "50%" }} >
-                    <p style={{ margin: "4%", padding: "0%" }}>{this.state.correctAnswers}</p>
-                    <p style={{ margin: "4%", padding: "0%" }}>{this.state.incorrectAnswers}</p>
-                    <p style={{ margin: "4%", padding: "0%" }}>{this.state.correctAnswers + this.state.incorrectAnswers === 0 ? "N/A" : (100 * this.state.correctAnswers / (this.state.correctAnswers + this.state.incorrectAnswers)).toFixed(2) + "%"}</p>
-                    <p style={{ margin: "4%", padding: "0%" }}>{this.state.score}</p>
-                    <p style={{ margin: "4%", padding: "0%" }}>{this.state.correctAnswers + this.state.incorrectAnswers === 0 ? "N/A" : (this.state.score / (this.state.correctAnswers + this.state.incorrectAnswers)).toFixed(2)}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Show settings popup */}
-            {this.state.showSettings ?
-            <SettingsPopup
-              closeSettings={this.closeSettings.bind(this)}
-              updateConfig={this.updateConfig.bind(this)}
-              numberDelay={this.state.numberDelay}
-              problemDelay={this.state.problemDelay}
-              timeForPoints={this.state.timeForPoints}
-            />
-            : null }
-          </div>
-        </BrowserView>
-        {/* Displays on mobile */}
-        <MobileView>
-          <p>This application is not available on mobile yet. Try again soon!</p>
-        </MobileView>
-      </>
-    );
-  }
-
-  // Resets user score
-  clearScore() {
-    this.setState({
-      correctAnswers: 0,
-      incorrectAnswers: 0,
-      score: 0,
-      clearScoreLocked: true,
-    })
-  }
-
-  // Returns new problem
-  getProblem() {
-    let numbers = [0, 0, 0, 0]
-
-    // Randomly deal 13 cards to 4 players
-    for (let i = 0; i < 13; i++) {
-      var nextPerson: number = Math.floor(Math.random() * 4)
-      numbers[nextPerson] = numbers[nextPerson] + 1
-    }
-
-    return numbers
-  }
-
-  // Initiates 10 new problems
-  async tenProblems() {
-    this.setState({ problemsRemaining: 9 }, () => { this.onStart() })
-  }
-
   // Starts new problem
   async onStart() {
     // Add keyboard listener
@@ -269,9 +94,17 @@ class App extends Component<{}, BridgeState> {
     this.setState({ answer: "?", answersLocked: false }, () => { this.startTime = new Date().getTime(); })
   }
 
-  // Set program to sleep
-  delay(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  // Returns new problem
+  getProblem() {
+    let numbers = [0, 0, 0, 0]
+
+    // Randomly deal 13 cards to 4 players
+    for (let i = 0; i < 13; i++) {
+      var nextPerson: number = Math.floor(Math.random() * 4)
+      numbers[nextPerson] = numbers[nextPerson] + 1
+    }
+
+    return numbers
   }
 
   // Process user answer
@@ -322,6 +155,175 @@ class App extends Component<{}, BridgeState> {
       }
     }
     return 1
+  }
+
+  // Initiates 10 new problems
+  async tenProblems() {
+    this.setState({ problemsRemaining: 9 }, () => { this.onStart() })
+  }
+
+  // Set program to sleep
+  delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  // Resets user score
+  clearScore() {
+    this.setState({
+      correctAnswers: 0,
+      incorrectAnswers: 0,
+      score: 0,
+      clearScoreLocked: true,
+    })
+  }
+  
+  openSettings() {
+    this.setState({ showSettings: true }, () => document.removeEventListener("keydown", this.keydown, false))
+  }
+
+  closeSettings() {
+    this.setState({ showSettings: false }, () => document.addEventListener("keydown", this.keydown, false))
+  }
+
+  updateConfig(numberDelay: number, problemDelay: number) {
+    this.setState({
+      numberDelay: numberDelay,
+      problemDelay: problemDelay,
+    })
+  }
+
+  // When key is pressed
+  keydown(event: any) {
+    let keyPressed = event.keyCode
+    // Only allow 0-9 and q,w,e,r
+    if (!this.state.answersLocked && keyPressed >= 48 && keyPressed <= 57) {
+      this.submit(keyPressed - 48)
+    }
+    if (!this.state.answersLocked && keyPressed === 81) {
+      this.submit(10)
+    }
+    if (!this.state.answersLocked && keyPressed === 87) {
+      this.submit(11)
+    }
+    if (!this.state.answersLocked && keyPressed === 69) {
+      this.submit(12)
+    }
+    if (!this.state.answersLocked && keyPressed === 82) {
+      this.submit(13)
+    }
+  }
+
+  render() {
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    const answerButtons = numbers.map((key) => {
+      return (
+        <button disabled={this.state.answersLocked} style={{ margin: "0.5%", height: "90%", width: "9%" }} key={key} onClick={() => this.submit(key)}>
+          {key}
+        </button>
+
+      )
+    })
+    let messageColor = this.state.message === "Correct!" ? "green" : "red"
+    let pointsForProblem = this.state.pointsForProblem === null ? "" : this.state.pointsForProblem + " points"
+    return (
+      <>
+        {/* Displays on PC  */}
+        <BrowserView>
+          <div style={{ position: "absolute", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "100%", width: "100%" }}>
+            <div style={{ alignContent: "center", width: "60%", height: "40%", display: "flex", flexDirection: "column", backgroundColor: "darkgray", border: "2px solid black" }}>
+              {/* Header */}
+              <div style={{ justifyContent: "space-between", alignItems: "center", width: "100%", height: "20%", display: "flex", flexDirection: "row", backgroundColor: "gray" }}>
+                <p>Basta Bridge | Counting Game :)</p>
+                {/* Settings button */}
+                <img style={{ padding: "3%", height: "45%" }} alt="settings button" src={gear} onClick={() => this.openSettings()} />
+              </div>
+
+              <hr style={{ width: "100%", margin: "0px", border: "1px solid black" }} />
+
+              <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "80%", width: "100%" }}>
+
+                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100%", width: "60%" }}>
+
+                  {/* Problem display */}
+                  <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "40%", width: "100%" }} >
+                    <div className="number-box">
+                      <h2>{this.state.num1}</h2>
+                    </div>
+                    <div className="number-box">
+                      <h2>{this.state.num2}</h2>
+                    </div>
+                    <div className="number-box">
+                      <h2>{this.state.num3}</h2>
+                    </div>
+                    <div className="number-box">
+                      <h2>{this.state.answer}</h2>
+                    </div>
+                  </div>
+
+                  {/* Answer buttons  */}
+                  <div style={{ margin: "0%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "15%", width: "100%" }}>
+                    {answerButtons}
+                  </div>
+
+                  {/* Message on answer */}
+                  <div style={{ margin: "0%", display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "25%", width: "100%" }}>
+                    <h2 style={{ color: messageColor }}>{this.state.message + " " + pointsForProblem}</h2>
+                  </div>
+
+                  {/* Control buttons */}
+                  <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "20%", width: "100%" }}>
+                    <button disabled={this.state.newProblemsLocked || this.state.problemsRemaining > 0} className="btn button" onClick={() => this.onStart()}>
+                      New problem
+                    </button>
+                    <button disabled={this.state.newProblemsLocked || this.state.problemsRemaining > 0} className="btn button" onClick={() => this.tenProblems()}>
+                      10 Problems
+                    </button>
+                    <button disabled={this.state.clearScoreLocked || this.state.problemsRemaining > 0} className="btn button" onClick={() => this.clearScore()}>
+                      Clear Score
+                    </button>
+                  </div>
+                </div>
+
+                {/* Stats display */}
+                <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", height: "100%", width: "40%" }} >
+                  {/* Left side */}
+                  <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "flex-end", height: "100%", width: "50%" }} >
+                    <p style={{ margin: "4%", padding: "0%" }}>Correct answers:</p>
+                    <p style={{ margin: "4%", padding: "0%" }}>Incorrect answers:</p>
+                    <p style={{ margin: "4%", padding: "0%" }}>Correct percentage:</p>
+                    <p style={{ margin: "4%", padding: "0%" }}>Total points:</p>
+                    <p style={{ margin: "4%", padding: "0%" }}>Average points:</p>
+                  </div>
+
+                  {/* Right side */}
+                  <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "flex-start", height: "100%", width: "50%" }} >
+                    <p style={{ margin: "4%", padding: "0%" }}>{this.state.correctAnswers}</p>
+                    <p style={{ margin: "4%", padding: "0%" }}>{this.state.incorrectAnswers}</p>
+                    <p style={{ margin: "4%", padding: "0%" }}>{this.state.correctAnswers + this.state.incorrectAnswers === 0 ? "N/A" : (100 * this.state.correctAnswers / (this.state.correctAnswers + this.state.incorrectAnswers)).toFixed(2) + "%"}</p>
+                    <p style={{ margin: "4%", padding: "0%" }}>{this.state.score}</p>
+                    <p style={{ margin: "4%", padding: "0%" }}>{this.state.correctAnswers + this.state.incorrectAnswers === 0 ? "N/A" : (this.state.score / (this.state.correctAnswers + this.state.incorrectAnswers)).toFixed(2)}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* Show settings popup */}
+            {this.state.showSettings ?
+              <SettingsPopup
+                closeSettings={this.closeSettings.bind(this)}
+                updateConfig={this.updateConfig.bind(this)}
+                numberDelay={this.state.numberDelay}
+                problemDelay={this.state.problemDelay}
+                timeForPoints={this.state.timeForPoints}
+              />
+              : null}
+          </div>
+        </BrowserView>
+        {/* Displays on mobile */}
+        <MobileView>
+          <p>This application is not available on mobile yet. Try again soon!</p>
+        </MobileView>
+      </>
+    );
   }
 }
 
